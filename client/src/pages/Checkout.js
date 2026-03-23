4760481e6dd469addb371a1641de77a370f2ca7c
Aminkhan12631 <111694894+Aminkhan12631@users.noreply.github.com>
2026-03-24 03:43:00 +0530

    Comment out token retrieval from localStorage
    Commented out the line fetching the token from localStorage.

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -72,7 +72,7 @@ const [address, setAddress] = useState("");
 
     try {
 
-      const token = localStorage.getItem("token");
+      //const token = localStorage.getItem("token");
 
 const res = await fetch("http://localhost:5000/api/orders/create", {
   method: "POST",
